70e2a9e4213ca948672e813fadde9dcd82f55c96
Matt Trask <matt.trask@insightglobal.net>
2015-11-15 11:34:10 -0500

    added test and src files

---
+++ tests/githubTest.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Phergie plugin for displaying Github Issues
+ * (rocketpastsix/phergie-irc-plugin-react-github)
+ *
+ * @link https://github.com/phergie/phergie-irc-plugin-react-github for the canonical source repository
+ * @copyright Copyright (c) 2015 Matt Trask (http://phergie.org)
+ * @license http://phergie.org/license Simplified BSD License
+ * @package Phergie\Irc\Plugin\React\TableFlip
+ */
+namespace Phergie\Irc\Tests\Plugin\React\Github;
+use Phake;
+use ReflectionMethod;
+use Phergie\Irc\Bot\React\EventQueueInterface as Queue;
+use Phergie\Irc\Plugin\React\Command\CommandEvent as Event;
+use Phergie\Irc\Plugin\React\Github;
+/**
+ * Tests for the Plugin class.
+ *
+ * @category Phergie
+ * @package Phergie\Irc\Plugin\React\Github
+ */
+class ScriptTest extends \PHPUnit_Framework_TestCase
+{
+}
+?>
